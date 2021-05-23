9504903a870afab6962ab1c5f8aad485c239cba6
matthasronnau@berkeley.edu <matthiasronnau@berkeley.edu>
2021-05-23 15:45:31 -0700

    Update 'filter_read_csv'

--- R/filter_read_csv.R
+++ R/filter_read_csv.R
@@ -1,7 +1,8 @@
 #' @title Filter Read CSV
 #' @description Iteratively reads in and filters a CSV file based on specified filters.
 #' @param path A file path to a local csv file.
-#' @param batch_size The size of each csv chunk to be read in.
+#' @param batch_size The size of each csv chunk to be read in. If not specified, then the data will be
+#' broken into 10 chunks by default.
 #' @param filters vector of filters to be applied. Each filter should start with the column name and
 #' NOT the name of the file; filter_read_csv will take care of this automatically. For example, if one
 #' of the filters was to be applied on the column "Age", the filter would be included in the vector
@@ -10,25 +11,26 @@
 #' @importFrom readr read_csv cols
 #' @export
 
-filter_read_csv <- function(path, batch_size = 100, filters){
+filter_read_csv <- function(path, batch_size, filters){
   check_filters(filters) #Ensure at least one filter is present
   file_nrow <- length(count.fields(path)) #Get the number of rows present in the data
-  size <- batch_size
+  size <- ifelse(is.null(batch_size), round(file_nrow / 10), batch_size)
   batches <- seq(from = 0, to = file_nrow + batch_size, by = size) #Prepare the batches based on the batch size and the overall file size
   dat = NULL #Initialize an empty dataframe
-  cols.names <- colnames(readr::read_csv(path, skip = batches[1], n_max = 0, col_types = readr::cols())) #Get column names from data
+  cols.names <- colnames(readr::read_csv(path, skip = batches[1], n_max = 0, col_types = readr::cols(), progress = FALSE)) #Get column names from data
   filter_length <- length(filters) #Count number of filters inputed
 
   for(i in 1:length(batches)){ #Iterate through every batch
     data_temp <- readr::read_csv(path, skip = batches[i], #Read in a batch_size amount of data
-                          n_max = batch_size, col_types = readr::cols())
+                          n_max = batch_size, col_types = readr::cols(), progress = FALSE)
     colnames(data_temp) <- cols.names #Set the column names of the batch
     for(j in 1:filter_length){ #Iterate through every filter
       data_temp <- subset(data_temp, eval(parse(text = paste("data_temp$", filters[j], sep = "")))) #Subset the data based on the given filters
     }
     dat <- rbind(dat, data_temp) #rbind the filtered data to the filtered data from previous batches
     rm(data_temp_filtered) #Remove the filtered batch
-    cat(round(batches[i] / size * 100, 2), "%    \r") #Print what percentage of the rows of the initial CSV file have been read and filtered through
+    cat(i, " of ", length(batches), " batches cleaned and read.", sep = "")
+    #cat(round(batches[i] / file_nrow * 100, 2), "%    \r") #Print what percentage of the rows of the initial CSV file have been read and filtered through
   }
   dat #Return the filtered data
 }
